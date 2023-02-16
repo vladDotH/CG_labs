6fcfdba29d664db5cf86fd7c220161ec7fb1d897
Vladislav <vladbolkunovv@gmail.com>
2023-02-16 22:33:56 +0300

    feat: control & gl bind

--- MainWindow.py
+++ MainWindow.py
@@ -7,6 +7,8 @@ from ControlPanel import ControlPanel
 from GLWidget import GLWidget
 from PrimitiveSelector import PrimitiveSelector
 
+from drawing import *
+
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -17,15 +19,32 @@ class MainWindow(QMainWindow):
         sp = QSplitter(self)
         sp.addWidget(self.glwidget)
         sp.addWidget(self.control)
-        sp.setStretchFactor(1, 1)
-        sp.setStretchFactor(2, 4)
+        sp.setStretchFactor(0, 1)
 
         self.control.primitiveSelector.primitiveSelected.connect(self.primitiveSelected)
+        self.control.recreate.clicked.connect(self.regenerate)
         self.setCentralWidget(sp)
         self.resize(800, 600)
 
+        self.drawers = dict([
+            (gl.GL_POINT, drawPoints),
+            (gl.GL_LINES, drawLines),
+            (gl.GL_LINE_STRIP, drawLinesStrip),
+            (gl.GL_LINE_LOOP, drawLinesLoop),
+            (gl.GL_TRIANGLES, drawTriangles),
+            (gl.GL_TRIANGLE_STRIP, drawTrianglesStrip),
+            (gl.GL_TRIANGLE_FAN, drawTrianglesFan),
+            (gl.GL_QUADS, drawQuads),
+            (gl.GL_QUAD_STRIP, drawQuadsStrip),
+            (gl.GL_POLYGON, drawPolygon),
+        ])
+        self.primitiveSelected(list(self.drawers.keys())[0])
+
     @QtCore.pyqtSlot(gl.Constant)
     def primitiveSelected(self, primitive):
-        print(primitive)
-        self.glwidget.color = (1, 0, 0)
+        self.glwidget.function = self.drawers[primitive]
+        self.glwidget.update()
+
+    @QtCore.pyqtSlot()
+    def regenerate(self):
         self.glwidget.update()
