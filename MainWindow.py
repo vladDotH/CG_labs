e5648dbf99157cae75c93b0ffdf914793368fafc
Vlad <vladbolkunovv@gmail.com>
2023-02-28 19:44:10 +0300

    feat: 2nd lab

--- MainWindow.py
+++ MainWindow.py
@@ -9,7 +9,7 @@ from drawing import *
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.setWindowTitle("0303 Болкунов В. О. Лабораторная работа № 1")
+        self.setWindowTitle("0303 Болкунов В. О. Лабораторная работа № 2")
         self.control = ControlPanel(self)
         self.glwidget = GLWidget(self)
         sp = QSplitter(self)
@@ -18,61 +18,59 @@ class MainWindow(QMainWindow):
         sp.setStretchFactor(0, 1)
 
         self.setCentralWidget(sp)
-        self.resize(800, 600)
+        self.resize(900, 600)
 
-        # Словарь количества генерируемых точек в одной фигуре
-        self.pointsCount = dict([
-            (gl.GL_POINTS, 1),
-            (gl.GL_LINES, 2),
-            (gl.GL_LINE_STRIP, 2),
-            (gl.GL_LINE_LOOP, 2),
-            (gl.GL_TRIANGLES, 3),
-            (gl.GL_TRIANGLE_STRIP, 3),
-            (gl.GL_TRIANGLE_FAN, 3),
-            (gl.GL_QUADS, 4),
-            (gl.GL_QUAD_STRIP, 4),
-            (gl.GL_POLYGON, 1),
-        ])
-
-        # Сгенерированные фигуры
-        self.objects = []
         # Выбранный примитив
         self.primitive = None
-        # Начальная инициализация
-        self.primitiveSelected(list(self.pointsCount.keys())[0])
+        # Задаём рендер-функцию
+        self.glwidget.function = self.renderFunction
+        # Обновление ползунков при изменении размеров окна
+        self.glwidget.viewPortResized.connect(self.onGLResize)
 
-        self.control.primitiveSelector.primitiveSelected.connect(self.primitiveSelected)
-        self.control.recreate.clicked.connect(self.regenerate)
-        self.control.objectsCount.valueChanged.connect(self.redraw)
-        self.control.pointsSize.valueChanged.connect(self.redraw)
-        self.control.lineWidth.valueChanged.connect(self.redraw)
+        c = self.control
+        for i in [c.scissorsX, c.scissorsY, c.scissorsH, c.scissorsW, c.scissorsH, c.transparencyRef]:
+            i.valueChanged.connect(self.redraw)
+        for i in [c.primitiveSelector, c.transparencyFunc, c.sFactor, c.dFactor]:
+            i.selectedSignal.connect(self.redraw)
 
-    # Установка опций рисования
-    @staticmethod
-    def setOptions(line: float, points: float):
+        # TODO fix shit:
+        self.onGLResize(9999, 9999)
+        c.scissorsW.setValue(c.scissorsW.maximum())
+        c.scissorsH.setValue(c.scissorsH.maximum())
+
+    @QtCore.pyqtSlot(int, int)
+    def onGLResize(self, w, h):
+        self.control.scissorsW.setMaximum(w)
+        self.control.scissorsH.setMaximum(h)
+        self.control.scissorsX.setMaximum(w)
+        self.control.scissorsY.setMaximum(h)
+
+    def renderFunction(self):
         # Размытие цвета между вершинами
         gl.glShadeModel(gl.GL_SMOOTH)
-        gl.glLineWidth(line)
-        gl.glPointSize(points)
+        gl.glLineWidth(10)
+        gl.glPointSize(10)
 
-    # Повторная генерация фигур
-    @QtCore.pyqtSlot()
-    def regenerate(self):
-        self.objects = generateFigures(self.pointsCount[self.primitive], self.control.objectsCount.maximum())
-        self.redraw()
+        # Отсечение
+        gl.glEnable(gl.GL_SCISSOR_TEST)
+        gl.glScissor(
+            self.control.scissorsX.value(), self.control.scissorsY.value(),
+            self.control.scissorsW.value(), self.control.scissorsH.value()
+        )
 
-    # Генерация фигур при изменении примитива
-    @QtCore.pyqtSlot(gl.Constant)
-    def primitiveSelected(self, primitive):
-        self.primitive = primitive
-        self.objects = generateFigures(self.pointsCount[primitive], self.control.objectsCount.maximum())
+        # Прозрачность
+        gl.glEnable(gl.GL_ALPHA_TEST)
+        gl.glAlphaFunc(
+            self.control.transparencyFunc.selectedObject,
+            self.control.transparencyRef.value() / self.control.transparencyRef.maximum()
+        )
 
-        # Рендер функция вызывает функцию установки опций и функцию рисования с заданными параметрами
-        self.glwidget.function = lambda: [
-            self.setOptions(self.control.lineWidth.value(), self.control.pointsSize.value()),
-            drawFigures(self.objects[:self.control.objectsCount.value()], self.primitive)
-        ]
-        self.redraw()
+        # Смешивание
+        gl.glEnable(gl.GL_BLEND)
+        gl.glBlendFunc(self.control.sFactor.selectedObject, self.control.dFactor.selectedObject)
+
+        # Отрисовка
+        renderers[self.control.primitiveSelector.selectedObject]()
 
     # Вызов обновления изображения
     @QtCore.pyqtSlot()
