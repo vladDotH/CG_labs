8487da53d31164fc6c00458ae3ab3f477be7111e
Vlad <vladbolkunovv@gmail.com>
2023-02-28 19:20:30 +0300

    feat: resize signal & viewport setting

--- GLWidget.py
+++ GLWidget.py
@@ -1,13 +1,20 @@
 from OpenGL import GL as gl
+from PyQt6 import QtCore
 from PyQt6.QtOpenGLWidgets import QOpenGLWidget
 
 
 # Виджет OpenGL
 class GLWidget(QOpenGLWidget):
+    viewPortResized = QtCore.pyqtSignal((int, int))
+
     def __init__(self, parent=None):
         super().__init__(parent)
         # Функция вызываемая в цикле отрисовки (при обновлениях)
         self.function = None
+
+    def resizeGL(self, w: int, h: int) -> None:
+        gl.glViewport(0, 0, w, h)
+        self.viewPortResized.emit(w, h)
 
     # Функция вызываемая перед любым обновлением
     def initializeGL(self):
