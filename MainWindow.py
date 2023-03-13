68b36580d9acdde156b132c1980de8995aab61c5
Vlad <vladbolkunovv@gmail.com>
2023-03-14 00:35:17 +0300

    feat: new main window

--- MainWindow.py
+++ MainWindow.py
@@ -9,7 +9,7 @@ from drawing import *
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.setWindowTitle("0303 Болкунов В. О. Лабораторная работа № 2")
+        self.setWindowTitle("0303 Болкунов В. О. Лабораторная работа № 3")
         self.control = ControlPanel(self)
         self.glwidget = GLWidget(self)
         sp = QSplitter(self)
@@ -20,58 +20,27 @@ class MainWindow(QMainWindow):
         self.setCentralWidget(sp)
         self.resize(900, 600)
 
-        # Выбранный примитив
-        self.primitive = None
         # Задаём рендер-функцию
         self.glwidget.function = self.renderFunction
-        # Обновление ползунков при изменении размеров окна
-        self.glwidget.viewPortResized.connect(self.onGLResize)
 
-        c = self.control
-        for i in [c.scissorsX, c.scissorsY, c.scissorsH, c.scissorsW, c.scissorsH, c.transparencyRef]:
-            i.valueChanged.connect(self.redraw)
-        for i in [c.primitiveSelector, c.transparencyFunc, c.sFactor, c.dFactor]:
-            i.selectedSignal.connect(self.redraw)
+        self.radiusFraction = 1000
 
-        #
-        self.onGLResize(9999, 9999)
-        c.scissorsW.setValue(c.scissorsW.maximum())
-        c.scissorsH.setValue(c.scissorsH.maximum())
-
-    # Обработка изменения размеров окна
-    @QtCore.pyqtSlot(int, int)
-    def onGLResize(self, w, h):
-        self.control.scissorsW.setMaximum(w)
-        self.control.scissorsH.setMaximum(h)
-        self.control.scissorsX.setMaximum(w)
-        self.control.scissorsY.setMaximum(h)
+        self.control.radius.setMaximum(self.radiusFraction)
+        self.control.iterations.setMaximum(100)
+        self.control.iterations.setTickInterval(1)
+        self.control.radius.valueChanged.connect(self.redraw)
+        self.control.iterations.valueChanged.connect(self.redraw)
 
     def renderFunction(self):
         # Размытие цвета между вершинами
         gl.glShadeModel(gl.GL_SMOOTH)
-        gl.glLineWidth(10)
+        gl.glLineWidth(3)
         gl.glPointSize(10)
-
-        # Отсечение
-        gl.glEnable(gl.GL_SCISSOR_TEST)
-        gl.glScissor(
-            self.control.scissorsX.value(), self.control.scissorsY.value(),
-            self.control.scissorsW.value(), self.control.scissorsH.value()
-        )
-
-        # Прозрачность
-        gl.glEnable(gl.GL_ALPHA_TEST)
-        gl.glAlphaFunc(
-            self.control.transparencyFunc.selectedObject,
-            self.control.transparencyRef.value() / self.control.transparencyRef.maximum()
-        )
-
-        # Смешивание
-        gl.glEnable(gl.GL_BLEND)
-        gl.glBlendFunc(self.control.sFactor.selectedObject, self.control.dFactor.selectedObject)
-
-        # Отрисовка
-        renderers[self.control.primitiveSelector.selectedObject]()
+        dots = generate(self.control.iterations.value(), self.control.radius.value() / self.radiusFraction)
+        drawLines(dots)
+        for level in dots:
+            for p in level:
+                drawCircle(self.control.radius.value() / self.radiusFraction / 2, p)
 
     # Вызов обновления изображения
     @QtCore.pyqtSlot()
