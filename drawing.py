e418fdb6601e527a2e133b3247e7fb170107ad17
Vlad <vladbolkunovv@gmail.com>
2023-03-14 00:51:54 +0300

    feat: new draw functions

--- drawing.py
+++ drawing.py
@@ -1,109 +1,50 @@
 from OpenGL import GL as gl
-import random
+import numpy as np
+
+angles = np.linspace(0, 2 * np.pi, 7)[:-1] + np.pi / 6
 
 
-def randomColor():
-    return [random.uniform(-1, 1) for i in range(3)]
-
-
-def drawPoints():
-    gl.glBegin(gl.GL_POINTS)
-    points = [
-        (0, 0), (0, 1), (1, 0), (-1, 0), (0, -1), (1, 1), (-1, -1), (-1, 1), (1, -1), (0.5, 0.5)
+def generate(n, r):
+    return [
+        [
+            i * r * np.array([np.cos(t), np.sin(t)]) for t in angles
+        ] for i in range(n)
     ]
-    for i in points:
-        gl.glColor3dv(randomColor())
-        gl.glVertex2dv(i)
-    gl.glEnd()
 
 
-def drawLines():
-    gl.glBegin(gl.GL_LINES)
-    lines = [
-        ((-0.5, 0), (0, 1)),
-        ((1, 0), (-1, 0.5)),
-        ((0.7, -1), (1, 1)),
-        ((-0.5, -0.7), (0.8, 0.5)),
-        ((-0.4, -0.7), (0.8, -0.5))
-    ]
-    for i in lines:
-        for p in i:
-            gl.glColor3dv(randomColor())
-            gl.glVertex2dv(p)
-    gl.glEnd()
+colors = [
+    [0.06, 0.28, 0.66],
+    [0.78, 0.0, 0.49],
+    [0.66, 0.94, 0.0],
+    [1.0, 0.65, 0.0]
+]
+
+circleAngles = np.linspace(0, 2 * np.pi, 50)
+circleVecs = [np.array([np.cos(t), np.sin(t)]) for t in circleAngles]
 
 
-def drawLineStrip():
+def drawCircle(r, p, color):
     gl.glBegin(gl.GL_LINE_STRIP)
-    ponts = [
-        (-0.5, 0), (-0.7, 0.8),
-        (0, 0.6), (1, 0.5),
-        (0.7, 0.8), (0.9, -0.8),
-        (-0.1, -0.7), (0, 0.95)
-    ]
-    for p in ponts:
-        gl.glColor3dv(randomColor())
-        gl.glVertex2dv(p)
+    gl.glColor3dv(color)
+    for a in circleVecs:
+        gl.glVertex2dv(p + a * r)
     gl.glEnd()
 
 
-def drawLineLoop():
-    gl.glBegin(gl.GL_LINE_LOOP)
-    ponts = [
-        (-0.5, 0), (-0.7, 0.8),
-        (0, 0.6), (1, 0.5),
-        (0.7, 0.8), (0.9, -0.8),
-        (-0.1, -0.7), (0, 0.95)
-    ]
-    for p in ponts:
-        gl.glColor3dv(randomColor())
-        gl.glVertex2dv(p)
-    gl.glEnd()
-
-
-def drawTriangles():
-    gl.glBegin(gl.GL_TRIANGLES)
-    triangles = [
-        ((-0.9, -0.5), (0.4, 0.4), (-0.2, -0.9)),
-        ((0.9, -0.5), (-0.4, 0.4), (0.2, -0.9)),
-        ((-0.4, 0.9), (0.4, 0.9), (0, -0.4)),
-    ]
-    colors = [(0.9, 0.2, 0.1, 0.4), (0.2, 0.7, 0.1, 0.5), (0.1, 0.2, 0.8, 0.6)]
-    for i, color in zip(triangles, colors):
-        gl.glColor4dv(color)
-        for p in i:
-            gl.glVertex2dv(p)
-    gl.glEnd()
-
-
-def drawTriangleStrip():
-    gl.glBegin(gl.GL_TRIANGLE_STRIP)
-    points = [
-        (-0.9, -0.5), (-0.3, 0.6), (-0.2, -0.9),
-        (0.1, -0.1), (0.7, -0.8), (0.8, 0.8), (-0.9, -0.3)
-    ]
-    colors = [
-        (0.9, 0.2, 0.1, 0.5), (0.9, 0.2, 0.1, 0.5), (0.9, 0.2, 0.1, 0.5),
-        (0.2, 0.7, 0.1, 0.5), (0.2, 0.7, 0.1, 0.5), (0.1, 0.2, 0.8, 0.5), (0.1, 0.2, 0.8, 0.5)
-    ]
-    for i, color in zip(points, colors):
-        gl.glColor4dv(color)
-        gl.glVertex2dv(i)
-    gl.glEnd()
-
-
-def drawTriangleFan():
-    gl.glBegin(gl.GL_TRIANGLE_FAN)
-    points = [
-        (0, 0), (-0.7, -0.8), (0, -0.9), (0.3, -0.4),
-        (0.9, -0.3), (0.3, 0.8), (-0.5, 0.8), (-0.1, -0.99)
-    ]
-    colors = [
-        (0.9, 0.2, 0.1, 0.5), (0.9, 0.2, 0.1, 0.5), (0.9, 0.2, 0.1, 0.5),
-        (0.2, 0.7, 0.1, 0.5), (0.2, 0.7, 0.1, 0.5), (0.1, 0.2, 0.8, 0.5),
-        (0.1, 0.2, 0.8, 0.5), (0.1, 0.2, 0.8, 0.5)
-    ]
-    for i, color in zip(points, colors):
-        gl.glColor4dv(color)
-        gl.glVertex2dv(i)
+def drawLines(dots):
+    gl.glBegin(gl.GL_LINES)
+    # Уровни
+    for i in range(1, len(dots)):
+        # Точки в уровне
+        for j in range(6):
+            gl.glColor3dv(colors[j % 4])
+            # Соединение точки с двумя другими на своём уровне
+            for k in range(1, 3):
+                gl.glVertex2dv(dots[i][j])
+                gl.glVertex2dv(dots[i][j - 2 * k])
+            # Соединение точки с двумя точками предыдущего уровня
+            gl.glVertex2dv(dots[i][j])
+            gl.glVertex2dv(dots[i - 1][j - 1])
+            gl.glVertex2dv(dots[i][j])
+            gl.glVertex2dv(dots[i - 1][(j + 1) % 6])
     gl.glEnd()
