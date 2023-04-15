a745a96492f7a956092035005b6cb7eca26942d9
Vlad <vladbolkunovv@gmail.com>
2023-04-15 16:25:08 +0300

    feat: frequency & amplitude control

--- ControlPanel.py
+++ ControlPanel.py
@@ -8,11 +8,23 @@ class ControlPanel(QWidget):
         super().__init__(parent)
         lt = QVBoxLayout(self)
         self.setLayout(lt)
+
+        drawLabel = QLabel('Рисование фрактала', self)
         levelsLabel = QLabel('Количество итераций', self)
         self.iterations = QSlider(Qt.Orientation.Horizontal, self)
         radiusLabel = QLabel('Расстояние между уровнями (масштаб)', self)
         self.radius = QSlider(Qt.Orientation.Horizontal, self)
+
+        shaderLable = QLabel('Парметры шейдера', self)
+        waveLable = QLabel('Частота волны', self)
+        self.waves = QSlider(Qt.Orientation.Horizontal, self)
+        amplitudeLable = QLabel('Амплитуда', self)
+        self.amplitude = QSlider(Qt.Orientation.Horizontal, self)
+
         #
-        for i in [levelsLabel, self.iterations, radiusLabel, self.radius]:
+        for i in [
+            drawLabel, levelsLabel, self.iterations, radiusLabel, self.radius,
+            shaderLable, waveLable, self.waves, amplitudeLable, self.amplitude
+        ]:
             lt.addWidget(i)
         lt.addStretch()
